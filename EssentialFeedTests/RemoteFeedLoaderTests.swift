c8102c80bc40c3f434b2730cd85bbf64346dc2d8
Denys Triasunov <denys.triasunov@gmail.com>
2024-05-06 21:27:36 +0300

    test: combine parameters captured by spy into a single type (tuple)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -61,17 +61,19 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
+                
+        private var messages = [(url: URL, completion: (Error?) -> Void)]()
         
-        var requestedURLs = [URL]()
-        var completions = [(Error?) -> Void]()
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
         
         func get(from url: URL, completion: @escaping (Error?) -> Void) {
-            requestedURLs.append(url)
-            completions.append(completion)
+            messages.append((url, completion))
         }
         
         func complete(with error: Error, atIndex index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 }
