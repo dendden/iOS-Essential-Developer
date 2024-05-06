e1d9cba930becbce6e4317834dd6fc506caba5dc
Denys Triasunov <denys.triasunov@gmail.com>
2024-05-06 22:19:40 +0300

    test: replace optional closure params with custom Result typealias

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,9 +7,11 @@
 
 import Foundation
 
+public typealias HTTPClientResult = Result<HTTPURLResponse, Error>
+
 public protocol HTTPClient {
         
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -28,11 +30,12 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error?) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            
+            switch result {
+            case .success:
                 completion(.invalidData)
-            }
-            else {
+            case .failure:
                 completion(.connectivity)
             }
         }
