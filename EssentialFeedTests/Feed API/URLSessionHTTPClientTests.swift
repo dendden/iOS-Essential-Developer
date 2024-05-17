92604346df15f760b133fadaf5f2ef9035333b1b
Denys Triasunov <denys.triasunov@gmail.com>
2024-05-17 11:40:20 +0300

    test: move start and stop intercepting requests to XCTestCase setUP() and tearDown() methods

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -28,9 +28,19 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_performsGetRequestFromURL() {
+    override func setUp() {
+        super.setUp()
         
         URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_getFromURL_performsGetRequestFromURL() {
         
         let url = URL(string: "https://any-url.com")!
         let exp = expectation(description: "Wait for request")
@@ -45,13 +55,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        
-        URLProtocolStub.startInterceptingRequests()
-        
+                
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(withData: nil, response: nil, error: error)
@@ -71,7 +78,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     // MARK: - Factory
