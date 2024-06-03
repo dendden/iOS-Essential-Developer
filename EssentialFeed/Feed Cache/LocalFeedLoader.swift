12bea626d4034474456414650288e9132235daea
Denys Triasunov <denys.triasunov@gmail.com>
2024-06-03 20:45:54 +0300

    add SaveResult type alias in place of hardcoded Error to protect code from potential breaking changes

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -9,6 +9,8 @@ import Foundation
 
 public final class LocalFeedLoader {
     
+    public typealias SaveResult = Error?
+    
     private let store: FeedStore
     private let currentDate: () -> Date
     
@@ -17,7 +19,7 @@ public final class LocalFeedLoader {
         self.currentDate = currentDate
     }
     
-    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void = { _ in }) {
+    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void = { _ in }) {
         
         store.deleteCachedFeed { [weak self] cacheDeletionError in
             
@@ -32,7 +34,7 @@ public final class LocalFeedLoader {
         }
     }
     
-    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         
         store.insert(items, timestamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
