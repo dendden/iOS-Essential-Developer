61ec047c82c7836b97a1c993c083372398bfe805
Denys Triasunov <dendden@users.noreply.github.com>
2026-05-07 23:24:16 +0300

    CoreDataFeedStore.retrieve() delivers found values on non-empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -13,12 +13,37 @@ public final class CoreDataFeedStore: FeedStore {
     private let context: NSManagedObjectContext
     
     public init(storeURL: URL, bundle: Bundle = .main) throws {
+        
         container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        
+        let context = self.context
+        context.perform {
+            do {
+                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+                request.returnsObjectsAsFaults = false
+                if let cache = try context.fetch(request).first {
+                    completion(.found(
+                        feed: cache.feed
+                            .compactMap { ($0 as? ManagedFeedImage) }
+                            .map {
+                                LocalFeedImage(id: $0.id,
+                                               description: $0.imageDescription,
+                                               location: $0.location,
+                                               url: $0.url)
+                            },
+                        timestamp: cache.timestamp))
+                }
+                else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
@@ -27,6 +52,26 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         
+        let context = self.context
+        context.perform {
+            do {
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = NSOrderedSet(array: feed.map { local in
+                    let managed = ManagedFeedImage(context: context)
+                    managed.id = local.id
+                    managed.imageDescription = local.description
+                    managed.location = local.location
+                    managed.url = local.url
+                    return managed
+                })
+                
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
 }
 
@@ -63,17 +108,19 @@ private extension NSManagedObjectModel {
     }
 }
 
+@objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
 
-    @NSManaged public var timestamp: Date?
-    @NSManaged public var feed: NSOrderedSet?
+    @NSManaged public var timestamp: Date
+    @NSManaged public var feed: NSOrderedSet
 }
 
+@objc(ManagedFeedImage)
 private class ManagedFeedImage: NSManagedObject {
     
-    @NSManaged public var id: UUID?
+    @NSManaged public var id: UUID
     @NSManaged public var imageDescription: String?
     @NSManaged public var location: String?
-    @NSManaged public var url: URL?
-    @NSManaged public var cache: ManagedCache?
+    @NSManaged public var url: URL
+    @NSManaged public var cache: ManagedCache
 }
